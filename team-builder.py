a9cfa0681739b4528857e1a196d8dd8330469d4b
Maarco921 <68565718+Maarco921@users.noreply.github.com>
2026-06-17 23:16:53 -0400

    able to create a team using input 6 times

--- team-builder.py
+++ team-builder.py
@@ -1,11 +1,19 @@
 import requests
 
-name = input("Enter a Pokemon name: ")
-response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
-if response.status_code == 404:
-    print(f"Pokemon '{name}' not found. Check your spelling.")
-else:
-    data = response.json()
-    types = data["types"]
-    for t in types:
-        print(t["type"]["name"])
+i=0
+team=[]
+while i < 6:
+    name = input("Enter a Pokemon name: ")
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+    if response.status_code == 404:
+        print(f"Pokemon '{name}' not found. Check your spelling.")
+    else:
+        data = response.json()
+        types = data["types"]
+        for t in types:
+            print(t["type"]["name"])
+        i = i + 1
+        team.append(name)
+        
+        
+print(team)
