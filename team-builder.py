d5f6d88fc0e57b0682382c14b269a8749ecba6c1
Maarco921 <68565718+Maarco921@users.noreply.github.com>
2026-06-17 23:00:29 -0400

    added an input feature

--- team-builder.py
+++ team-builder.py
@@ -1,8 +1,11 @@
 import requests
 
-response = requests.get("https://pokeapi.co/api/v2/pokemon/pikachu")
-data = response.json()
-
-types = data["types"]
-for t in types:
-    print(t["type"]["name"])
+name = input("Enter a Pokemon name: ")
+response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+if response.status_code == 404:
+    print(f"Pokemon '{name}' not found. Check your spelling.")
+else:
+    data = response.json()
+    types = data["types"]
+    for t in types:
+        print(t["type"]["name"])
