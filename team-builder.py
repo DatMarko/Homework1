e593330815abbcc726c188cb426d56b3cbd04ae0
Maarco921 <68565718+Maarco921@users.noreply.github.com>
2026-06-21 21:58:48 -0400

    fetch weaknesses for each pokemon type

--- team-builder.py
+++ team-builder.py
@@ -2,17 +2,28 @@ import requests
 
 i=0
 team=[]
+weaknesses=[]
 while i < 6:
     name = input("Enter a Pokemon name: ")
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+
     if response.status_code == 404:
         print(f"Pokemon '{name}' not found. Check your spelling.")
     else:
         data = response.json()
         types = data["types"]
         for t in types:
-            print(t["type"]["name"])
-        i = i + 1
+            type_name = t["type"]["name"]
+            print("Pokemon types are: " + t["type"]["name"])
+            response2 = requests.get(f"https://pokeapi.co/api/v2/type/{type_name}")
+            data2 = response2.json()
+            
+            weakness = data2["damage_relations"]["double_damage_from"]
+        
+            for t in weakness:
+                print("weakness is: " + t["name"])
+            
+
         team.append(name)
         
         
